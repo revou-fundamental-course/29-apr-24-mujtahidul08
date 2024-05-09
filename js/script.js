9cf793f6871126fa596421e2380edb827c011100
mujtahidul08 <mujahaq@gmail.com>
2024-05-10 00:00:08 +0700

    initial configuration

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let inputNama = document.getElementById('nama')
+
+// document.getElementById('submit').addEventListener("click", function(){
+//     const nama = inputNama.value
+//     if (nama == ''){
+//         document.getElementById("error-name").innerHTML = "Nama tidak boleh kosong!!"
+//     }
+//     else{
+//         document.getElementById('nama').innerHTML = nama
+//     }
+// })
+
+
